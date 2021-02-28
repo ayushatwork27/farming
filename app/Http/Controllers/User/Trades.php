f954413779ac1a9683bedd24ca74f8ab23bef0e4
ayush_agrawal <ayush.agrawal@corediagnostics.in>
2021-02-28 12:49:08 +0530

    local

---
+++ app/Http/Controllers/User/Trades.php
@@ -0,0 +1,34 @@
+ Trades
+
+ 	trade_id
+ 	crop_id
+ 	quantity
+ 	area
+ 	accepected_rate
+
+ 	package = [silver 20 /gold 24/normal 10]
+ 	actual_price = 20
+
+ 	submit()
+
+
+Crops
+	crop_id
+	name
+	category
+	active
+	timestamp
+	deleted_at
+	gold
+	silver
+	normal
+
+Crop  (list) 
+		(New)
+		(Edit)
+		(Delete)
+
+	
+
+
+
